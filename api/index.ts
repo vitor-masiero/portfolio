c32f7a0511cbcad34488b0e66dc3036018aef445
José Vitor <masierojosevitor456@gmail.com>
2025-12-06 13:49:22 -0300

    again

--- api/index.ts
+++ api/index.ts
@@ -4,10 +4,4 @@ import serverless from "serverless-http";
 
 const app = createServer();
 
-import fs from "fs";
-import path from "path";
-
-console.log("Files in /var/task/dist/server:");
-console.log(fs.readdirSync(path.resolve("./dist/server")));
-
 export default serverless(app);
